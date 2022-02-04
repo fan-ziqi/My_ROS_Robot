a5250088c8aa5de74ad33f23997bfcb827215307
FZQ's VM Ubuntu 20.04 <fanziqi614@gmail.com>
2022-02-04 11:56:37 +0800

    fix some

--- ROS-WorkSpace/ROS2-Robot-WS/src/build/robot/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/fzq614/My_ROS_Robot/ROS-WorkSpace/ROS2-Robot-WS/src/build/robot/robot" "TARGETS" "robot" "DESTINATION" "lib/robot")
--- ROS-WorkSpace/ROS2-Robot-WS/src/build/serial_test/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/fzq614/My_ROS_Robot/ROS-WorkSpace/ROS2-Robot-WS/src/build/serial_test/serial_test" "TARGETS" "serial_test" "DESTINATION" "lib/serial_test")
